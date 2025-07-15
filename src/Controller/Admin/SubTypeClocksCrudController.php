34aa121de869c4a2239be299eff6f997dac4afcd
ZugastiKevin <rochambeaut@gmail.com>
2025-07-15 12:13:47 +0200

    add: all admin

--- src/Controller/Admin/SubTypeClocksCrudController.php
+++ src/Controller/Admin/SubTypeClocksCrudController.php
@@ -3,10 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\SubTypeClocks;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class SubTypeClocksCrudController extends AbstractCrudController
 {
@@ -15,14 +17,17 @@ class SubTypeClocksCrudController extends AbstractCrudController
         return SubTypeClocks::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            AssociationField::new('typeClocks')
+            ->setFormTypeOptions([
+                'choice_label'=>'label',
+            ]),
+            TextField::new('label'),
+            TextareaField::new('story'),
+            TextareaField::new('subStory'),
         ];
     }
-    */
 }
